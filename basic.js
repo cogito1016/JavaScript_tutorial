c852657dd9c7a6a1979223087058e15d104efb38
KJH <cogito1016@naver.com>
2018-10-04 01:30:51 +0900

    Declaring object 'var' type

--- basic.js
+++ basic.js
@@ -1,9 +1,14 @@
 
-			var now = new Date();
-			document.write(now);
+var now = new Date();
+document.write(now);
 
-			var x;
-			x="Hello world";
-			document.write(x);
-			x=100;
-			document.write(x);
+var x;
+x="Hello world";
+document.write(x);
+x=100;
+document.write(x);
+
+var car = {model:"bmz",color:"red",hp:100};
+document.write(car.model+"<br>");
+document.write(car.color+"<br>");
+document.write(car.hp+"<br>");
