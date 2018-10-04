7bb23be1998600b555ee2d694c01f7c4876fba4a
KJH <cogito1016@naver.com>
2018-10-04 14:48:38 +0900

    Using prompt keyword and '+'

--- basic.js
+++ basic.js
@@ -1,4 +1,5 @@
 
+/*
 var now = new Date();
 document.write(now);
 
@@ -12,3 +13,26 @@ var car = {model:"bmz",color:"red",hp:100};
 document.write(car.model+"<br>");
 document.write(car.color+"<br>");
 document.write(car.hp+"<br>");
+
+var temp;
+document.write(temp+"<br>");
+
+if(false === 0)
+	document.write("This is true");
+else
+	document.write("This is false <br>");
+
+
+document.write(a);
+var a=2;
+*/
+var x,y;
+var input;
+
+input = prompt("정수입력","정수로");
+x=parseInt(input);
+
+input = prompt("정수입력","정수로");
+y=parseInt(input);
+ 
+document.write(x+y+"<br>");
